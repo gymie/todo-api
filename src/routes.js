5c389c4230937c910b44e4e38af1c09cb4b1140b
Jimmy <6257063+gymie@users.noreply.github.com>
2022-04-06 16:34:26 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const {
+  addTodoHandler,
+  getTodoByIdHandler,
+  getAllTodoHandler,
+  updateTodoByIdHandler,
+  deleteTodoHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => 'Selamat datang di API Todo',
+  },
+  {
+    method: 'POST',
+    path: '/todos',
+    handler: addTodoHandler,
+  },
+  {
+    method: 'GET',
+    path: '/todos/{id}',
+    handler: getTodoByIdHandler,
+  },
+  {
+    method: 'GET',
+    path: '/todos',
+    handler: getAllTodoHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/todos/{id}',
+    handler: updateTodoByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/todos/{id}',
+    handler: deleteTodoHandler,
+  },
+];
+
+module.exports = { routes };
